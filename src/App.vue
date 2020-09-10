4e976f7dc627f62a1d866414f98917b2029aaed4
leo <863249346@qq.com>
2020-09-10 11:15:54 +0800

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="valueChange"/>
+    <a-button type="primary" @click="addList">添加事项</a-button>
+
+    <a-list bordered :dataSource="statusChangeList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="(e)=>{checkedChange(e,item.id)}">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeList(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{leftNum}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="status==='all' ? 'primary' : 'default'" @click="statusChange('all')">全部</a-button>
+          <a-button :type="status==='undone' ? 'primary' : 'default'" @click="statusChange('undone')">未完成</a-button>
+          <a-button :type="status==='done' ? 'primary' : 'default'" @click="statusChange('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clear">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+/* 按需导入vuex组件 */
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  /* 获取数据 */
+  created () {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    /* 映射为computed属性 */
+    ...mapState(['list', 'inputValue', 'nextId', 'status']),
+    ...mapGetters(['leftNum', 'statusChangeList'])
+  },
+  methods: {
+    /* 映射为methods函数 */
+    ...mapMutations(['getLists', 'inputChange']),
+    ...mapActions(['getList']),
+    /* 输入框值改变触发 */
+    valueChange (e) {
+      this.$store.commit('inputChange', e.target.value)
+    },
+    /* 删除列表数据 */
+    removeList (id) {
+      this.$store.commit('removeLists', id)
+    },
+    /* 增加列表数据 */
+    addList () {
+      if (this.inputValue.trim().length <= 0) {
+        this.$message.warning('请输入todo')
+      } else {
+        this.$store.commit('addLists')
+      }
+    },
+    /* 复选框点击触发 */
+    checkedChange (e, id) {
+      const params = {
+        id: id,
+        done: e.target.checked
+      }
+      this.$store.commit('checkChange', params)
+    },
+    /* 清除已完成项目 */
+    clear () {
+      this.$store.commit('clearDone')
+    },
+    // 完成状态切换
+    statusChange (status) {
+      this.$store.commit('statusListChange', status)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    padding: 10px;
+  }
+
+  .my_ipt {
+    width: 500px;
+    margin-right: 10px;
+  }
+
+  .dt_list {
+    width: 500px;
+    margin-top: 10px;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,91 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    /* 列表数据 */
+    list: [],
+    /* 输入框数据 */
+    inputValue: 'aaa',
+    /* 后续数据ID值 */
+    nextId: 5,
+    /* 状态值 */
+    status: 'all'
   },
   mutations: {
+    /* 获取列表数据 */
+    getLists (state, list) {
+      state.list = list
+    },
+    /* 输入框数据改变 */
+    inputChange (state, list) {
+      state.inputValue = list
+    },
+    /* 删除列表数据 */
+    removeLists (state, id) {
+      const index = state.list.findIndex(item => {
+        return item.id === id
+      })
+      state.list.splice(index, 1)
+    },
+    /* 增加列表数据 */
+    addLists (state) {
+      const newObj = { id: state.nextId, done: false, info: state.inputValue }
+      state.list.push(newObj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    /* 复选框点击触发 */
+    checkChange (state, params) {
+      const index = state.list.findIndex(item => {
+        return item.id === params.id
+      })
+      state.list[index].done = params.done
+    },
+    /* 清除已完成项目 */
+    clearDone (state) {
+      state.list = state.list.filter(item => {
+        return item.done === false
+      })
+    },
+    /* 完成状态切换 */
+    statusListChange (state, status) {
+      state.status = status
+    }
   },
   actions: {
+    /* 异步从静态资源目录获取list数据 */
+    getList (context) {
+      axios.get('/list.json').then(data => {
+        context.commit('getLists', data.data)
+      })
+    }
   },
   modules: {
+  },
+  getters: {
+    /* 包装计算剩余的未完成个数 */
+    leftNum (state) {
+      return state.list.filter(item => {
+        return item.done === false
+      }).length
+    },
+    /* 包装状态切换后的列表数据 */
+    statusChangeList (state) {
+      if (state.status === 'all') {
+        return state.list
+      } else if (state.status === 'done') {
+        return state.list.filter(item => {
+          return item.done === true
+        })
+      } else if (state.status === 'undone') {
+        return state.list.filter(item => {
+          return item.done === false
+        })
+      }
+    }
   }
 })
